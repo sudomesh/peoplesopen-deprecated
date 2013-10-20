46362a811650f4125e7e871f519123a20e8fda5c
wrought <mattsenate@gmail.com>
2013-10-20 04:24:50 -0700

    first commit, adds child theme

---
+++ footer.php
@@ -0,0 +1,30 @@
+			<footer role="contentinfo">
+			
+					<div class="twelve columns">
+
+						<div class="row">
+
+							<nav class="ten columns clearfix">
+								<?php bones_footer_links(); ?>
+							</nav>
+
+							<p class="attribution two columns"><a href="https://sudoroom.org/wiki/Mesh" id="credit" title="Initialized by Sudo Mesh, a Sudo Room project">Sudo Mesh</a></p>
+
+						</div>
+
+					</div>
+					
+			</footer> <!-- end footer -->
+		
+		</div> <!-- end #container -->
+		
+		<!--[if lt IE 7 ]>
+  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+		<![endif]-->
+		
+		<?php wp_footer(); // js scripts are inserted using this function ?>
+
+	</body>
+
+</html>
---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Extends functions.php from parent theme */
+
+remove_filter('nav_menu_css_class', 'remove_menu_styles', 10, 2);
+
+?>
