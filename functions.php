e5f2ddc843b94b5cc14092a1413ac9ecfe738ea0
wrought <mattsenate@gmail.com>
2013-10-21 17:03:46 -0700

    adds greater mobile menu depth, custom function

--- functions.php
+++ functions.php
@@ -2,6 +2,19 @@
 
 /* Extends functions.php from parent theme */
 
-remove_filter('nav_menu_css_class', 'remove_menu_styles', 10, 2);
+
+function peoples_bones_mobile_nav() {
+        // display the wp3 menu if available
+    wp_nav_menu(
+        array(
+                'menu' => 'mobile_nav', /* menu name */
+                'menu_class' => 'side-nav tabs vertical',
+                'theme_location' => 'main_nav', /* where in the theme it's assigned */
+                'container_class' => 'mobile-nav-container', /* container tag */
+                'fallback_cb' => 'bones_main_nav_fallback', /* menu fallback */
+                'depth' => '2'
+        )
+    );
+}
 
 ?>
