f6f870e213cef3f1591f6fcd4a104b8b5e18a8cd
wrought <mattsenate@gmail.com>
2013-10-22 00:03:25 -0700

    fixes jquery dependency, updates font function

--- functions.php
+++ functions.php
@@ -2,20 +2,19 @@
 
 /* Extends functions.php from parent theme */
 
-function kill_old_fonts() {
+function replace_fonts() {
     wp_dequeue_style( 'open-sans' );
 }
 
-add_action( 'wp_enqueue_scripts', 'kill_old_fonts', 11 );
+add_action( 'wp_enqueue_scripts', 'replace_fonts', 11 );
 
 function replace_jQuery() {
-    wp_dequeue_script('jquery');
     wp_deregister_script('jquery'); // initiate the function
     wp_register_script('jquery', get_template_directory_uri().'/javascripts/jquery.min.js', __FILE__, false, '1.7.2', true); // register the local file
     wp_enqueue_script('jquery'); // enqueue the local file
 }
 
-add_action('wp_enqueue_scripts', 'replace_jQuery', 11 );
+add_action('wp_enqueue_scripts', 'replace_jQuery', 9 );
 
 
 function peoples_bones_mobile_nav() {
