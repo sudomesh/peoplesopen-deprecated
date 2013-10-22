bb2b7cfdf0c3df7d4aebe98b5da039de809f0c4d
wrought <mattsenate@gmail.com>
2013-10-21 22:01:30 -0700

    attempts to remove links to http resources favoring local https

--- functions.php
+++ functions.php
@@ -2,6 +2,21 @@
 
 /* Extends functions.php from parent theme */
 
+function kill_old_fonts() {
+    wp_dequeue_style( 'open-sans' );
+}
+
+add_action( 'wp_enqueue_scripts', 'kill_old_fonts', 11 );
+
+function replace_jQuery() {
+    wp_dequeue_script('jquery');
+    wp_deregister_script('jquery'); // initiate the function
+    wp_register_script('jquery', get_template_directory_uri().'/javascripts/jquery.min.js', __FILE__, false, '1.7.2', true); // register the local file
+    wp_enqueue_script('jquery'); // enqueue the local file
+}
+
+add_action('wp_enqueue_scripts', 'replace_jQuery', 11 );
+
 
 function peoples_bones_mobile_nav() {
         // display the wp3 menu if available
